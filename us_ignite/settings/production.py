b034d31d05dd971fbf7e49a81ebc295cc5d4ad76
Walter Lau <walter@fissionstrategy.com>
2017-01-09 21:23:23 -0800

    add whitenoise package for static file serving

--- us_ignite/settings/production.py
+++ us_ignite/settings/production.py
@@ -281,7 +281,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
+
     'whitenoise.middleware.WhiteNoiseMiddleware',
 
     "mezzanine.core.request.CurrentRequestMiddleware",
