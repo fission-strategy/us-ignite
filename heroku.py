6b9866efa27cd22e00f2ffd23ad0b5d2bbfa9c38
Walter Lau <walter@fissionstrategy.com>
2017-01-09 21:30:09 -0800

    update heroku.py to add execute_from_command_line

--- heroku.py
+++ heroku.py
@@ -12,9 +12,10 @@ middleware here, or combine a Django application with an application of another
 framework."""
 
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-
+from django.core.management import execute_from_command_line
 
 if __name__ == "__main__":
 
@@ -23,6 +24,7 @@ if __name__ == "__main__":
     settings_module = "%s.production" % real_project_name("us_ignite.settings")
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "us_ignite.settings")
 
+    execute_from_command_line(sys.argv)
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
