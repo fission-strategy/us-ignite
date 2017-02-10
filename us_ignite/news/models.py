d2b401cea24ec9477aaedc88eb8cee747649a229
Walter Lau <walter@fissionstrategy.com>
2017-02-10 10:45:18 -0800

    remove taggit tag from news app

--- us_ignite/news/models.py
+++ us_ignite/news/models.py
@@ -2,8 +2,6 @@ from __future__ import unicode_literals
 
 from django.db import models
 from mezzanine.blog.models import BlogPost
-from taggit.managers import TaggableManager
-from us_ignite.apps.models import TaggedCategory
 from mezzanine.utils.models import upload_to
 from mezzanine.core.fields import FileField
 from django.utils.translation import ugettext_lazy as _
@@ -14,7 +12,6 @@ class NewsPost(BlogPost):
     excerpt = models.TextField(blank=True, null=True)
     image = FileField(_("File"), max_length=255, format="Image",
         upload_to=upload_to("news.NewsPost.image", "galleries"), null=True, blank=True)
-    category_tags = TaggableManager(through=TaggedCategory, blank=True, verbose_name='Categories Tags')
     category_keywords = KeywordsField(verbose_name=_("Categories"))
 
 
