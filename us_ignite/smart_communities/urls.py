fb7635b9e8fb14b9d4379ad4b8fe2b29d3676487
Walter Lau <walter@fissionstrategy.com>
2017-02-15 19:18:51 -0800

    adding resources templates, and update embed video version in pip

--- us_ignite/smart_communities/urls.py
+++ us_ignite/smart_communities/urls.py
@@ -1,7 +1,23 @@
 from django.conf.urls import  url
 from . import views
+from us_ignite.apps import views as apps_views
+from us_ignite.testbeds import views as testbeds_views
+from us_ignite.resources import views as resources_views
 
 
 urlpatterns = [
     url(r'^$', views.home_view, name='smart_communities_home'),
+
+    url(r'^applications/$', apps_views.app_list, {'program': 'smart-gigabit-communities'}, name='sgc_apps'),
+    url(r'^applications/add/$', apps_views.app_add, {'program': 'smart-gigabit-communities'}, name='sgc_apps_add'),
+
+    url(r'^testbeds/$', testbeds_views.testbed_list, name='testbed_list'),
+    url(r'^testbeds/(?P<slug>[-\w]+)/$', testbeds_views.testbed_detail, name='testbed_detail'),
+    url(r'^testbeds/(?P<slug>[-\w]+)/locations.json$', testbeds_views.testbed_locations_json,
+        name='testbed_locations_json'),
+
+    url(r'^resources/$', resources_views.resource_list, name='sgc_resource_list'),
+    url(r'^resources/add/$', resources_views.resource_add, name='sgc_resource_add'),
+    url(r'^resources/?(P<slug>[-\w]+)/$', resources_views.resource_detail, name='sgc_resource_detail'),
+    url(r'^resources/?(P<slug>[-\w]+)/edit/$', resources_views.resource_edit, name='sgc_resource_edit'),
 ]
