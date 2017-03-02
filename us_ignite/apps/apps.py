cfe0b99f67a387587bce99f26c83289652796344
Walter Lau <walter@fissionstrategy.com>
2017-03-02 15:16:53 -0800

    search WIP and fixing homepage hub list

--- us_ignite/apps/apps.py
+++ us_ignite/apps/apps.py
@@ -1,7 +1,11 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
-
+from watson import search as watson
 
 class AppsConfig(AppConfig):
-    name = 'apps'
+    name = 'us_ignite.apps'
+
+    def ready(self):
+        Application = self.get_model("Application")
+        watson.register(Application)
---
+++ us_ignite/search/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+import views
+
+
+urlpatterns = [
+    url(r'^$', views.search, name='search'),
+    url(r'^apps/$', views.search_apps, name='search_apps'),
+    # url(r'^events/$', 'search_events', name='search_events'),
+    # url(r'^hubs/$', 'search_hubs', name='search_hubs'),
+    # url(r'^actionclusters/$', 'search_actionclusters', name='search_actionclusters'),
+    # url(r'^orgs/$', 'search_organizations', name='search_organizations'),
+    # url(r'^resources/$', 'search_resources', name='search_resources'),
+    # url(r'^tags.json$', 'tag_list', name='tag_list'),
+]
