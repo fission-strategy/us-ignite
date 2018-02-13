038e68454cc91f65f20ced6d6be09935739d298a
George Holmes <george@georgeholmesii.com>
2018-02-12 18:11:13 -0600

    update to reverse pitch page

--- us_ignite/smart_gigabit_communities/views.py
+++ us_ignite/smart_gigabit_communities/views.py
@@ -1,15 +1,17 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.template.response import TemplateResponse
 from models import *
 import random
+
 # Create your views here.
 
 
 def reverse_pitch(request):
     # "Static" text here. OPTIONAL
-    intro = "US Ignite Will Convene Reverse Pitch Events in Nine Communities Throughout the United States "
-    desc = "From Arizona to Texas to Vermont, US Ignite will co-host and sponsor multiple reverse pitch events for technologists and entrepreneurs throughout February and March. This reverse pitch competition is for makers, developers and entrepreneurs developing ultra high-bandwidth hardware, software and applications that want to impact their local community. Entrepreneurs will hear pitches from civic organizations and then compete for part of a prize pool of cash and in-kind services. US Ignite will co-sponsor these events in nine of the organization's Smart Gigabit Communities, which are a network of communities nationwide that have each committed to leverage next-generation smart city and Internet technologies to keep pace with the world's rapidly changing, technology-driven economy."
-
+    intro = "US Ignite Will Convene Reverse Pitch Events Throughout the United States "
+    desc = "US Ignite’s Reverse Pitch Competition turns the tables on the traditional competition by pitching challenges to you.<br><br>Local businesses, city governments, and civic organizations within Smart Gigabit Communities have the opportunity to present their problem statements to the wider community to curate fresh solutions.<br><br>Our Reverse Pitch events are focused on empowering the local change makers and idea generators out there to ideate and develop innovative gigabit applications solutions. Using our supportive platform, these ideas have a place to launch and have the chance to bring transformative benefits to their communities.<br><br>This year, US Ignite will co-sponsor up to 10 Reverse Pitch competitions.<br><br>Visit the webpages below to find out more information!"
+    
     random_int = random.uniform(0.1, 2.0)
     pitch_list = Pitch.objects.filter(active=True).order_by('order').all()
     context = {
